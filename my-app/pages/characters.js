7d6b830f4c799c96801a4ccde9ba1b23b9796b90
Blake Green <bgreen@intervision.com>
2020-08-28 12:12:18 -0700

    update imports

--- my-app/pages/characters.js
+++ my-app/pages/characters.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Link from 'next/Link'
+import React from 'react';
+import Layout from '../components/Layout';
+import Link from 'next/Link';
 
 export default function characters({character}) {
     console.log(character)
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,5 +1,5 @@
-import Layout from '../components/Layout'
-import Link from 'next/Link'
+import Layout from '../components/Layout';
+import Link from 'next/Link';
 
 export default function Home({characters}) {
   return (
